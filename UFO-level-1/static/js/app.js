35da95905f5594db338ae6807ea0c8f4513de7df
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-06-18 13:43:13 -0700

    initial commit

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,8 @@
+// from data.js
+var tableData = data;
+
+// append a table to webpage, add new rows of data for each UFO sighting
+//create column for date/time, city, state, country, shape, and comment
+//use a date form to listen for events and search thru date/time columns
+
+
