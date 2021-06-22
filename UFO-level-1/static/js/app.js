77ea1dcb4c600e5cc25cad03decb175409cbb3e9
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-06-22 15:51:12 -0700

    able to see data on webpage

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,4 +5,18 @@ var tableData = data;
 //create column for date/time, city, state, country, shape, and comment
 //use a date form to listen for events and search thru date/time columns
 
+//use d3 to select table
+
+var tbody= d3.select("tbody");
+
+tableData.forEach(function(sighting){
+    console.log(sighting)
+    var row= tbody.append("tr");
+
+    Object.values(sighting).forEach(function(value){
+        console.log(value);
+        var cell=row.append("td");
+        cell.text(value);
+    });
+});
 
