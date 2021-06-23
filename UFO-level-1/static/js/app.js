0fe588eb9281c2a4dea24f439616bcf444e3847e
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-06-22 22:15:15 -0700

    code is running smoothly, just not showing filter by date info

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,24 +1,47 @@
 // from data.js
 var tableData = data;
 
-// append a table to webpage, add new rows of data for each UFO sighting
-//create column for date/time, city, state, country, shape, and comment
-//use a date form to listen for events and search thru date/time columns
-
-//use d3 to select table
-
 var tbody= d3.select("tbody");
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
 
-tableData.forEach(function(sighting){
-    console.log(sighting)
-    var row= tbody.append("tr");
 
-    Object.values(sighting).forEach(function(value){
-        console.log(value);
-        var cell=row.append("td");
-        cell.text(value);
+//load data, iterate thru rows
+function loadData(){
+    tableData.forEach(function(sighting){
+        console.log(sighting)
+        var row= tbody.append("tr");
+
+        Object.values(sighting).forEach(function(value){
+            console.log(value);
+            var cell=row.append("td");
+            cell.text(value);
+        });
     });
-});
+}
+loadData();
+
+button.on("click", runEnter);
+form.on("click", runEnter);
+
+//create function to filter by date
+function runEnter(){
+    //prevent page
+    d3.event.preventDefault();
+    var inputDate= d3.select("#datetime").property("value");
+    console.log(inputDate);
+    d3.select("label>span").text(inputDate);
+
+    //filter data by date
+    var filterByDate= tableData.filter(element => element.datetime ===inputDate);
+    console.log(filterByDate);
+    //clear table
+    tbody.html("");
+
+    loadData(filterByDate);
+    
+}
 
 
-;
+
+
